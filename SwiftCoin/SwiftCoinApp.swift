71fc17fbd065f60f24b06ef6c482960269da5213
herisusanto <herisusanto.app@gmail.com>
2023-12-31 22:25:01 +0700

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by loratech on 31/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
